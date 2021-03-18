e4116b9124518ce97baf928d6ae45158df441b28
Baseris-de <ismail.baser@stud.hs-hannover.de>
2021-03-18 23:06:49 +0100

    Ich denke, das ist für eine Klinik fertig.

--- bewertungen/bewertungen/spiders/test.py
+++ bewertungen/bewertungen/spiders/test.py
@@ -1,10 +1,11 @@
 import scrapy
 
 
+
 class TestSpider(scrapy.Spider):
     name = 'test'
     allowed_domains = ['klinikbewertungen.de']
-    start_urls = ['https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-kreis-und-stadtkrankenhaus-alfeld']
+    start_urls = ['https://www.klinikbewertungen.de/klinik-forum/erfahrung-mit-krankenhaus-marienstift-braunschweig']
 
     def parse(self, response):
         next_page = response.xpath("//a[@class='raquo button']/@href").get()
@@ -30,42 +31,87 @@ class TestSpider(scrapy.Spider):
             else:
                 fachbereich = review.xpath(".//span[@class='right']/text()").get()
 
-            gesamtzufriedenheit = ((review.xpath(".//section/dl/dd/img/@class").get()).split("-"))[1]
+            gesamtzufriedenheit = review.xpath(".//section/dl/dd/img/@class").get()
             text_gesamtzufriedenheit = review.xpath(".//section/dl/dd/text()").get()
 
-            q_der_beratung = ((review.xpath(".//section/dl/dd[2]/img/@class").get()).split("-"))[1]
+            if gesamtzufriedenheit:
+                gesamtzufriedenheit = (gesamtzufriedenheit.split("-"))[1]
+                text_gesamtzufriedenheit = text_gesamtzufriedenheit
+            else:
+                gesamtzufriedenheit = 'Kein Info'
+                text_gesamtzufriedenheit = 'Kein Info'
+
+            q_der_beratung = review.xpath(".//section/dl/dd[2]/img/@class").get()
             text_q_der_beratung = review.xpath(".//section/dl/dd[2]/text()").get()
 
-            m_behandlung = ((review.xpath(".//section/dl/dd[3]/img/@class").get()).split("-"))[1]
+            if q_der_beratung:
+                q_der_beratung = (q_der_beratung.split("-"))[1]
+                text_q_der_beratung = text_q_der_beratung
+            else:
+                q_der_beratung = 'Kein Info'
+                text_q_der_beratung = 'Kein Info'
+
+            m_behandlung = review.xpath(".//section/dl/dd[3]/img/@class").get()
             text_m_behandlung = review.xpath(".//section/dl/dd[3]/text()").get()
 
-            v_und_ableufe = ((review.xpath(".//section/dl/dd[4]/img/@class").get()).split("-"))[1]
+            if m_behandlung:
+                m_behandlung = (m_behandlung.split("-"))[1]
+                text_m_behandlung = text_m_behandlung
+            else:
+                m_behandlung = 'Kein Info'
+                text_m_behandlung = 'Kein Info'
+
+            v_und_ableufe = review.xpath(".//section/dl/dd[4]/img/@class").get()
             text_v_und_ableufe = review.xpath(".//section/dl/dd[4]/text()").get()
 
-            a_und_gestaltung = ((review.xpath(".//section/dl/dd[5]/img/@class").get()).split("-"))[1]
+            if v_und_ableufe:
+                v_und_ableufe = (v_und_ableufe.split("-"))[1]
+                text_v_und_ableufe = text_v_und_ableufe
+            else:
+                v_und_ableufe = 'Kein Info'
+                text_v_und_ableufe = 'Kein Info'
+
+            a_und_gestaltung = review.xpath(".//section/dl/dd[5]/img/@class").get()
             text_a_und_gestaltung = review.xpath(".//section/dl/dd[5]/text()").get()
 
-            if (gesamtzufriedenheit=='null'):
-                gesamtzufriedenheit='Kein Info'
+            
+            if a_und_gestaltung:
+                a_und_gestaltung = (a_und_gestaltung.split("-"))[1]
+                text_a_und_gestaltung = text_a_und_gestaltung
+            else:
+                a_und_gestaltung = 'Kein Info'
+                text_a_und_gestaltung = 'Kein Info'
 
-            if (q_der_beratung=='null'):
-                q_der_beratung='Kein Info'
-
-            if (m_behandlung=='null'):
-                m_behandlung='Kein Info'
-
-            if (v_und_ableufe=='null'):
-                v_und_ableufe='Kein Info'
-
-            if (a_und_gestaltung=='null'):
-                a_und_gestaltung='Kein Info'
 
             pro = review.xpath(".//section[@class='report']/dl/dd[1]/text()").get()
             kontra = review.xpath(".//section[@class='report']/dl/dd[2]/text()").get()
             krankheitsbild = review.xpath(".//section[@class='report']/dl/dd[3]/text()").get()
-            erfahrungsbericht = review.xpath(".//section[@class='report']/dl[2]/dd/p/text()").getall()
+            privatpatient = review.xpath(".//section[@class='report']/dl/dd[4]/text()").get()
+            erfahrungsbericht = review.xpath(".//section[@class='report']/dl[2]/dd/p/text()").get()
+
+            nutzer = 'Kein Info'
+                        
+            if review.xpath(".//div[@class='meta']/span[@itemprop='author']/text()").get():
+                nutzer = review.xpath(".//div[@class='meta']/span[@itemprop='author']/text()").get()
+            elif review.xpath(".//div[@class='meta']/a/span[@itemprop='author']/text()").get():
+                nutzer = review.xpath(".//div[@class='meta']/a/span[@itemprop='author']/text()").get()
+
+            meta = review.xpath(".//div[@class='meta']/text()").getall()
+            berichtet_als = (((meta[2].strip()).split('|'))[0]).strip()
+            behandlungsjahr = (((meta[2].strip()).split('|'))[1]).strip()
+
+            daumen = 'Kein Info'
+            if review.xpath(".//img[@class='js-tooltip']").get():
+                daumen = ((review.xpath(".//img[@class='js-tooltip']/@src").get()).split('/'))[4]
+                if daumen == 'icon-recommend-me-fill.png':
+                    daumen = 'Daumen hoch'
+                else:
+                    daumen = 'Daumen runter'
+            
 
             
+
+            #print((((xx[2].strip()).split('|')[0])).strip())
 
             yield{
                 'Name der Klinik':klinik_name,
@@ -86,6 +132,11 @@ class TestSpider(scrapy.Spider):
                 'Pro':pro,
                 'Kontra':kontra,
                 'Krankheitsbild':krankheitsbild,
-                'Erfahrungsbericht':erfahrungsbericht
+                'Privatpatient':privatpatient,
+                'Erfahrungsbericht':erfahrungsbericht,
+                'Nutzername':nutzer,
+                'Berichtet als':berichtet_als,
+                'Behandlungsjahr':behandlungsjahr,
+                'Daumen':daumen
                 
             }
