827cf6b9316e685778551c856d041e72d80daf70
Baseris-de <ismail.baser@stud.hs-hannover.de>
2021-03-18 16:38:50 +0100

    new Column added

--- bewertungen/bewertungen/spiders/test.py
+++ bewertungen/bewertungen/spiders/test.py
@@ -30,11 +30,62 @@ class TestSpider(scrapy.Spider):
             else:
                 fachbereich = review.xpath(".//span[@class='right']/text()").get()
 
-           
+            gesamtzufriedenheit = ((review.xpath(".//section/dl/dd/img/@class").get()).split("-"))[1]
+            text_gesamtzufriedenheit = review.xpath(".//section/dl/dd/text()").get()
+
+            q_der_beratung = ((review.xpath(".//section/dl/dd[2]/img/@class").get()).split("-"))[1]
+            text_q_der_beratung = review.xpath(".//section/dl/dd[2]/text()").get()
+
+            m_behandlung = ((review.xpath(".//section/dl/dd[3]/img/@class").get()).split("-"))[1]
+            text_m_behandlung = review.xpath(".//section/dl/dd[3]/text()").get()
+
+            v_und_ableufe = ((review.xpath(".//section/dl/dd[4]/img/@class").get()).split("-"))[1]
+            text_v_und_ableufe = review.xpath(".//section/dl/dd[4]/text()").get()
+
+            a_und_gestaltung = ((review.xpath(".//section/dl/dd[5]/img/@class").get()).split("-"))[1]
+            text_a_und_gestaltung = review.xpath(".//section/dl/dd[5]/text()").get()
+
+            if (gesamtzufriedenheit=='null'):
+                gesamtzufriedenheit='Kein Info'
+
+            if (q_der_beratung=='null'):
+                q_der_beratung='Kein Info'
+
+            if (m_behandlung=='null'):
+                m_behandlung='Kein Info'
+
+            if (v_und_ableufe=='null'):
+                v_und_ableufe='Kein Info'
+
+            if (a_und_gestaltung=='null'):
+                a_und_gestaltung='Kein Info'
+
+            pro = review.xpath(".//section[@class='report']/dl/dd[1]/text()").get()
+            kontra = review.xpath(".//section[@class='report']/dl/dd[2]/text()").get()
+            krankheitsbild = review.xpath(".//section[@class='report']/dl/dd[3]/text()").get()
+            erfahrungsbericht = review.xpath(".//section[@class='report']/dl[2]/dd/p/text()").getall()
+
+            
+
             yield{
                 'Name der Klinik':klinik_name,
                 'bewertungen':bewertungen.strip(),
                 'Title':review_title,
                 'Datum der Bewertung':review_date,
-                'fachbereich':fachbereich.strip()
+                'fachbereich':fachbereich.strip(),
+                'Gesamtzufriedenheit':gesamtzufriedenheit,
+                'Textuell Gesamtzufriedenheit':text_gesamtzufriedenheit.strip(),
+                'Qualität der Beratung':q_der_beratung,
+                'Textuell Qualität der Beratung':text_q_der_beratung.strip(),
+                'Mediz. Behandlung':m_behandlung,
+                'Textuell Mediz. Behandlung':text_m_behandlung.strip(),
+                'Verwaltung und Abläufe':v_und_ableufe,
+                'Textuell Verwaltung und Abläufe':text_v_und_ableufe.strip(),
+                'Ausstattung und Gestaltung':a_und_gestaltung,
+                'Textuell Ausstattung und Gestaltung':text_a_und_gestaltung.strip(),
+                'Pro':pro,
+                'Kontra':kontra,
+                'Krankheitsbild':krankheitsbild,
+                'Erfahrungsbericht':erfahrungsbericht
+                
             }
