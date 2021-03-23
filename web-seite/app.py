940f2928c5cfb4f5f8b87e6407a39408eca2d6ff
Baseris-de <ismail.baser@stud.hs-hannover.de>
2021-03-23 21:51:35 +0100

    output_csv added and dropdownlist changed.

--- web-seite/app.py
+++ web-seite/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+reviews = pd.read_csv("output.csv")
 
 @app.route('/')
 
 def index():
-    return render_template("index.html")
+    clinics = reviews["Name der Klinik"].unique()
+
+    return render_template("index.html", clinics=clinics)   
 
 if __name__=="__main__":
     app.run(debug=True)
