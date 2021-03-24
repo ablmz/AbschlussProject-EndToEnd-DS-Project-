2370a4b98bbcea747640a342d091ea4cd275139a
Baseris-de <ismail.baser@stud.hs-hannover.de>
2021-03-24 21:03:08 +0100

    changed. with .lower() function

--- pro_flask/w_cloud.py
+++ pro_flask/w_cloud.py
@@ -30,7 +30,7 @@ def get_wcloud(df,name):
     blob_string=(" ").join(result)
     blob = TextBlobDE(blob_string)
     result = blob.tokens
-    
+    result = result.lower()
     german_stop_words = stopwords.words('german')
 
     filtered_sentence = [] 
@@ -39,8 +39,8 @@ def get_wcloud(df,name):
         if w not in german_stop_words: 
             filtered_sentence.append(w)
 
-    result=(" ").join(filtered_sentence)
-    wordcloud = WordCloud(background_color="white",width = 500, height = 500).generate(result)
+    result2=(" ").join(filtered_sentence)
+    wordcloud = WordCloud(background_color="white",width = 500, height = 500).generate(result2)
     # plt.figure(figsize=(15,8))
     plt.imshow(wordcloud)
     plt.axis("off")
@@ -52,3 +52,5 @@ clinics = df["Name der Klinik"].unique()
 
 for clinic in clinics:
     get_wcloud(df,clinic)
+
+# print(stopwords.words('german'))
