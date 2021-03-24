7c965766f5ec25c7f30ea99adfb7543283bbafca
Baseris-de <ismail.baser@stud.hs-hannover.de>
2021-03-24 22:13:12 +0100

    wordcloud added in index

--- pro_flask/app.py
+++ pro_flask/app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 import pandas as pd
+from slugify import slugify
 
 app = Flask(__name__)
 
 daten = pd.read_csv("output.csv")
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     clinics = daten["Name der Klinik"].unique()
-    return render_template("index.html", clinics=clinics)
+    if request.method == 'POST':
+        clinic_name = request.form.get('clinic_name')
+        image = slugify(clinic_name)+'.png'
+        return render_template("index.html", clinics=clinics, image=image )
+    else:
+        return render_template("index.html", clinics=clinics)
+
 
 if __name__ == '__main__':
     app.run()
