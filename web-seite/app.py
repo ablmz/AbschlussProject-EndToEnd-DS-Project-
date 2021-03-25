d822f74e0ef812f9e2693b6d9b5ffda23413cec1
Mrym <meryemonbasi5@gmail.com>
2021-03-25 16:24:52 +0100

    button richtig funktioniert

--- web-seite/app.py
+++ web-seite/app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 app = Flask(__name__)
 
 reviews = pd.read_csv("output.csv")
+clinics = reviews["Name der Klinik"].unique()
 
 @app.route('/')
 
 def index():
-    clinics = reviews["Name der Klinik"].unique()
+    
 
     return render_template("index.html", clinics=clinics)   
 
+@app.route('/ergebnisse', methods=["GET","POST"])
+
+def ergebnisse():
+    select = request.form.get("klinikNameList")
+    print(select)
+    return render_template("ergebnisse.html", clinics=clinics, klinikName=str(select)) 
+    
+
+
 if __name__=="__main__":
     app.run(debug=True)
+
+#C:/xampp/htdocs/abschluss/Final-Project/web-seite
